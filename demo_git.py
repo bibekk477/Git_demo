93cc6f87982e6a30b3903efba1acfd1837c4fb25
bibekk477 <114281368+bibekk477@users.noreply.github.com>
2025-11-27 21:33:29 +0545

    Introduce syntax error for Jenkins build testing
    Added a print statement to introduce a syntax error for testing Jenkins build failure.

--- demo_git.py
+++ demo_git.py
@@ -1,3 +1,5 @@
 print("this is original file before auto github repo pooling")
 print("this is a demo for git repo auto pooling ie automatic building in respose to git repo changes")
 print("testing for one more time git repo auto pooling in jenkins")
+print("
+introducing the sytax error leading to build fail in jenkins")
