a8c3c0122ffe98bcff45317e0fec0dd9f8c15082
Bibek Karki <bibekk477@gmail.com>
2025-12-11 11:20:44 +0545

    addded new line to demo.py

--- demo_git.py
+++ demo_git.py
@@ -1,5 +1,9 @@
 print("this is original file before auto github repo pooling")
-print("this is a demo for git repo auto pooling ie automatic building in respose to git repo changes")
+print(
+    "this is a demo for git repo auto pooling ie automatic building in respose to git repo changes"
+)
 print("testing for one more time git repo auto pooling in jenkins")
-print("introducing the sytax error leading to build fail in jenkins \nNow correcting the error")
-
+print(
+    "introducing the sytax error leading to build fail in jenkins \nNow correcting the error"
+)
+print("this is from another branch ie feature/demo")
